73974adecde407435a850f1b82b5fd927a5bb151
347247864@qq.com <linxu.dou@vcg.com>
2019-03-17 22:15:01 +0800

    初始化项目

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * @file app.js
+ * @author swan
+ */
+App({
+    onLaunch(options) {
+        // do something when launch
+    },
+    onShow(options) {
+        // do something when show
+    },
+    onHide() {
+        // do something when hide
+    },
+    api:'https://zuowen.jupeixun.cn/'
+});
