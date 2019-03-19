d760f7bb358897140d96f983d395cd2cd47a06b6
linxu.dou <linxu.dou@vcg.com>
2019-03-19 18:05:43 +0800

    完整版本

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,122 +5,7 @@ Page({
       logo:'../../images/logo.png',
       banner:"../../images/banner.png",
     },
-    classes:[
-      {
-        title:'小学作文',
-        banner:[
-          {
-            bannerText:'小雨伞小雨伞小雨伞小雨伞小雨伞小雨伞',
-            bannerUrl:'../../images/微信图片_201903151528191.jpg',
-          },
-          {
-            bannerText:'小雨伞',
-            bannerUrl:'../../images/微信图片_201903151528191.jpg',
-          }
-        ],
-        list:[
-          {
-            listTitle:"美丽的太阳花1",
-            listTime:"03-15",
-          },
-           {
-            listTitle:"美丽的太阳花美丽的太阳花美丽的太阳花美丽的太阳花",
-            listTime:"03-15",
-          },
-           {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-           {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          }, {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          }
-          , {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-        ]
-      },
-     {
-        title:'初中作文',
-        banner:[
-          {
-            bannerText:'小雨伞',
-            bannerUrl:'../../images/微信图片_201903151528191.jpg',
-          },
-          {
-            bannerText:'小雨伞',
-            bannerUrl:'../../images/微信图片_201903151528191.jpg',
-          }
-        ],
-        list:[
-          {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-           {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-           {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-           {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          }, {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          }
-          , {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-        ]
-      },
-      {
-        title:'高中作文',
-        banner:[
-          {
-            bannerText:'小雨伞',
-            bannerUrl:'../../images/微信图片_201903151528191.jpg',
-          },
-          {
-            bannerText:'小雨伞',
-            bannerUrl:'../../images/微信图片_201903151528191.jpg',
-          }
-        ],
-        list:[
-          {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-           {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-           {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-           {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          }, {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          }
-          , {
-            listTitle:"美丽的太阳花",
-            listTime:"03-15",
-          },
-        ]
-      },
-    ]
+    classes:[],
   },
   changeUrl(e){
      swan.navigateTo({url:e.currentTarget.dataset.pointUrl});
@@ -143,17 +28,14 @@ Page({
           'content-type': 'application/json' // 默认值
       },
       success: function (res) {
-        console.log(res);
         if(res.statusCode === 200 && res.data.status === 200){
-          console.log(1);
           var _data = res.data.data;
           _this.setData({
             classes:_data,
             loading:false,
           });
         }
-       swan.hideLoading();
-          
+        swan.hideLoading();
       },
       fail: function (err) {
           console.log('错误码：' + err.errCode);
--- pages/list/list.js
+++ pages/list/list.js
@@ -1,70 +1,77 @@
 
 Page({
   data:{
-    crumbs:{
-      name:'小学作文'
-    },
-    lists:[
-      {
-        title:'一只神奇的狗',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      },
-      {
-        title:'一只神奇的狗',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      },
-      {
-        title:'一只神奇的狗',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      },
-      {
-        title:'一只神奇的狗',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      },
-      {
-        title:'一只神奇的狗',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      },
-      {
-        title:'一只神奇的狗',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      },
-      {
-        title:'一只神奇的狗',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      },
-      {
-        title:'一只神奇的狗',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      },
-      {
-        title:'一只神奇的狗',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      },
-    ]
+    loading:true,
+    name:'',
+    lists:[],
+    id:'',
+    page:1,
   },
+  returnIndex(e){
+       swan.navigateBack();
+    },
   changeUrl(e){
      swan.navigateTo({url:e.currentTarget.dataset.pointUrl});
   },
   onReachBottom:function(){
-    console.log(1);
-    var _lists = this.data.lists;
-    _lists.push( {
-        title:'一只神奇的狗1',
-        abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
-        time:'03-17'
-      });
-      this.setData({
-        lists:_lists,
-      })
+    // console.log(1);
+    // var _lists = this.data.lists;
+    // _lists.push( {
+    //     title:'一只神奇的狗1',
+    //     abstract:'我发现窗外有一只黑白相间的狗。它的头是黑色的，身体是白色的，还带着黑色小斑点，尾巴短短的，像一条小辫子，走路的时候总喜欢把尾巴甩来甩去，它还有一双灵敏的耳朵，有时候往下垂，有时候竖起耳朵听旁边有没有动静。它一会儿东张西望，一会儿在旁边跳来跳去，像个淘气的小朋友。就在我转',
+    //     time:'03-17'
+    //   });
+    //   this.setData({
+    //     lists:_lists,
+    //   })
+    //进行翻页操作
+    const {id,page} = this.data;
+    this.getLists(id,page)
+    
+  },
+  getLists(id,page){
+    swan.showLoading({
+        title: '数据加载中',
+        mask: true
+    });
+    var _this = this;
+    var appInstance = getApp();
+    swan.request({
+      url: appInstance.api + 'bdprogram/list/c'+ id +'/',
+      data:{
+        id,
+        page
+      },
+      method: 'GET',
+      dataType: 'json',
+      header: {
+          'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        if(res.statusCode === 200 && res.data.status === 200){
+          var _data = res.data.data;
+           let text = [];
+            //去除当中的&nbsp;
+           _data.list && _data.list.forEach((lists,index)=>{
+                _data.list[index].text = lists.text.replace(/&nbsp;/g, "");
+            });
+          _this.setData({
+            lists:_this.data.lists.concat(_data.list),
+            name:_data.name,
+            loading:false,
+            id:id,
+            page:_this.data.page += 1,
+          });
+        }
+        swan.hideLoading();
+      },
+      fail: function (err) {
+          console.log('错误码：' + err.errCode);
+          console.log('错误信息：' + err.errMsg);
+      }
+    });
+  },
+  onLoad:function(ops){
+    this.getLists(ops.id,1);
   }
 });
